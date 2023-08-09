f758e6ebe636a992f66756030aec17d220c0666f
Mary <omota4fidelity@gmail.com>
2023-08-09 23:53:38 +0100

    completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 8;
+
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 100);
+const mod = num3 % num4;
+const max = Math.max(num2, num3, num4);
